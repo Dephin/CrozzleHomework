acb76c249c9fa61bae5d37bb90be1ec08597bea5
Huanglin <huanglin@nengmao.net>
2018-10-01 16:47:20 +0800

    修复main文件

--- CrozzleApplication/Program.cs
+++ CrozzleApplication/Program.cs
@@ -7,30 +7,19 @@ namespace CrozzleApplication
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-//        [STAThread]
+        // <summary>
+        // The main entry point for the application.
+        // </summary>
+        [STAThread]
         static void Main()
         {
-            //            #region Library
-            //            ClassLibrary1.Class1.log();
-            //            #endregion
-            //
-            //            Application.EnableVisualStyles();
-            //            Application.SetCompatibleTextRenderingDefault(false);
-            //            Application.Run(new CrozzleViewerForm());
-
-
-            Config.NonIntersectingLetterPoints = new int[] { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-            Config.IntersectingLetterPoints = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1 };
-            MagicBoard CrozzleBoard = new MagicBoard(10, 15); // Dynamic Crozzle board - 10rows 15cols
-            List<Word> Wordlist = new List<Word>() { new Word("AAOAT"), new Word("AARON"), new Word("ELIZIBETH") };
-            string Expected = "AARON";
-
-            // Act
-            List<ActiveWord> BestWord = CrozzleBoard.GetBestWord(Wordlist);
-            string Actual = BestWord[0].String;
+                        #region Library
+                        ClassLibrary1.Class1.log();
+                        #endregion
+            
+                        Application.EnableVisualStyles();
+                        Application.SetCompatibleTextRenderingDefault(false);
+                        Application.Run(new CrozzleViewerForm());
         }
     }
 }
--- CrozzleApplication/Test_MaxScoreCrozzle.cs
+++ CrozzleApplication/Test_MaxScoreCrozzle.cs
@@ -3,12 +3,12 @@ using CrozzleApplication.GenerateCrozzle;
 
 namespace CrozzleApplication
 {
-    //[TestClass]
+    [TestClass]
     public class Test_MaxScoreCrozzle
     {
 
 
-        public  void Test_BestWord(string[] args)
+        public void Test_BestWord()
         {
 //          Arrange
             Config.NonIntersectingLetterPoints = new int[] { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -22,29 +22,7 @@ namespace CrozzleApplication
             string Actual = BestWord[0].String;
 
             // Assert
-//            Assert.AreEqual(Expected, Actual);
+            Assert.AreEqual(Expected, Actual);
         }
-
-
-
-
-//        [TestMethod]
-//        public void GetBestWord_EmptyGridBoard_Test()
-//        {
-//            // Arrange
-//            Config.NonIntersectingLetterPoints = new int[] { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-//            Config.IntersectingLetterPoints = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1 };
-//            MagicBoard CrozzleBoard = new MagicBoard(10,15); // Dynamic Crozzle board - 10rows 15cols
-//            List<Word> Wordlist = new List<Word>() { new Word("SCOTT"), new Word("AARON"), new Word("ELIZIBETH") };
-//            string Expected = "AARON";
-//
-//            // Act
-//            List<ActiveWord> BestWord = CrozzleBoard.GetBestWord(Wordlist);
-//            string Actual = BestWord[0].String;
-//
-//            // Assert
-//            Assert.AreEqual(Expected, Actual);
-//
-//        }
     }
 }
