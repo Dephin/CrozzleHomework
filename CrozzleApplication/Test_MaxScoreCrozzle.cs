f25e89282dcaa072c34781377e09dcae1a6244a4
Dephin <hlzyjfeng@gmail.com>
2018-10-01 16:46:02 +0800

    Revert "取消測試文件注釋"
    This reverts commit d1f5375cc937843ef963201627d652e77509803f.

--- CrozzleApplication/Test_MaxScoreCrozzle.cs
+++ CrozzleApplication/Test_MaxScoreCrozzle.cs
@@ -3,7 +3,7 @@ using CrozzleApplication.GenerateCrozzle;
 
 namespace CrozzleApplication
 {
-    [TestClass]
+    //[TestClass]
     public class Test_MaxScoreCrozzle
     {
 
@@ -22,8 +22,29 @@ namespace CrozzleApplication
             string Actual = BestWord[0].String;
 
             // Assert
-            Assert.AreEqual(Expected, Actual);
+//            Assert.AreEqual(Expected, Actual);
         }
 
+
+
+
+//        [TestMethod]
+//        public void GetBestWord_EmptyGridBoard_Test()
+//        {
+//            // Arrange
+//            Config.NonIntersectingLetterPoints = new int[] { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+//            Config.IntersectingLetterPoints = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1 };
+//            MagicBoard CrozzleBoard = new MagicBoard(10,15); // Dynamic Crozzle board - 10rows 15cols
+//            List<Word> Wordlist = new List<Word>() { new Word("SCOTT"), new Word("AARON"), new Word("ELIZIBETH") };
+//            string Expected = "AARON";
+//
+//            // Act
+//            List<ActiveWord> BestWord = CrozzleBoard.GetBestWord(Wordlist);
+//            string Actual = BestWord[0].String;
+//
+//            // Assert
+//            Assert.AreEqual(Expected, Actual);
+//
+//        }
     }
 }
